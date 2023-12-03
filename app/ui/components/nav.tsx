b5b683b5243f92ed3a1e4ecc6d3a96503981646d
Kelmith <fayekelmith@gmail.com>
2023-12-03 12:01:37 +0530

    to continue navigation configuration

--- app/ui/components/nav.tsx
+++ app/ui/components/nav.tsx
@@ -13,34 +13,39 @@ const Navigation = () => {
   console.log(`Theme : ${theme}`);
   return (
     <nav className="bg-white text-black dark:bg-slate-800 dark:text-white ">
-      <Sidhra />
-      <ul className="flex w-full">
-        <li>
-          <Link href="/articles"> articles</Link>
-        </li>
-        <li>
-          <Link href="/record"> Achievements</Link>
-        </li>
-        <li>
-          <Link href="/about"> About Me</Link>
-        </li>
-        <li>
-          <button
-            className=" hover:caret-pink-500"
-            onClick={() => handleTheme()}
-          >
-            {theme === "light" ? (
-              <div>
-                <MoonIcon className="w-8 h-8 text-slate-800" />
-              </div>
-            ) : (
-              <div>
-                <SunIcon className="w-8 h-8 text-blank" />
-              </div>
-            )}
-          </button>
-        </li>
-      </ul>
+      <div className="flex max-w-screen-xl justify-between items-center">
+        <Sidhra />
+
+        <div>
+          <ul className="flex w-full">
+            <li>
+              <Link href="/articles"> articles</Link>
+            </li>
+            <li>
+              <Link href="/record"> Achievements</Link>
+            </li>
+            <li>
+              <Link href="/about"> About Me</Link>
+            </li>
+            <li>
+              <button
+                className=" hover:caret-pink-500"
+                onClick={() => handleTheme()}
+              >
+                {theme === "light" ? (
+                  <div>
+                    <MoonIcon className="w-8 h-8 text-slate-800" />
+                  </div>
+                ) : (
+                  <div>
+                    <SunIcon className="w-8 h-8 text-blank" />
+                  </div>
+                )}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
