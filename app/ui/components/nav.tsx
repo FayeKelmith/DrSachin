d1ef7dc00b7a0d2e51a9c91005c434b6b1f06e7e
fayekelmith <faye.12222883@lpu.in>
2023-12-05 10:26:38 +0530

    toggling done

--- app/ui/components/nav.tsx
+++ app/ui/components/nav.tsx
@@ -1,23 +1,51 @@
 "use client";
 import Sidhra from "@/app/ui/sidhra-logo";
 import Link from "next/link";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import {
+  MoonIcon,
+  SunIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 const Navigation = () => {
   const { theme, setTheme } = useTheme();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   const handleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
   console.log(`Theme : ${theme}`);
   return (
     <nav className="bg-white text-black dark:bg-slate-800 dark:text-white ">
-      <div className="flex max-w-screen-xl justify-between items-center">
+      <div className="flex max-w-screen-xl justify-between items-center flex-wrap mx-auto p-4">
         <Sidhra />
+        <button
+          className="md:hidden inline-flex hover:bg-gray-100 dark:hover:bg-slate-700 rounded-md focus:outline-none"
+          onClick={toggleNav}
+        >
+          {/* Mobile view Nav */}
+          <span className="">
+            {isNavOpen ? (
+              <XMarkIcon className="w-10 h-10" />
+            ) : (
+              <Bars3Icon className="w-10 h-10" />
+            )}
+          </span>
+        </button>
 
-        <div>
-          <ul className="flex w-full">
+        {/* Desktop view Nav */}
+        <div
+          className={`md:block justify-center md:w-auto ${
+            isNavOpen ? "block" : "hidden"
+          }`}
+        >
+          <ul className="flex flex-col md:flex-row">
             <li>
               <Link href="/articles"> articles</Link>
             </li>
@@ -49,6 +77,5 @@ const Navigation = () => {
     </nav>
   );
 };
-// this is a comment
 
 export default Navigation;
