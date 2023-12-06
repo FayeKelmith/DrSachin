8c1abb63456c5455f367f8140f47ff6d3850c1ee
fayekelmith <faye.12222883@lpu.in>
2023-12-06 14:15:16 +0530

    shifting

--- app/error.jsx
+++ app/error.jsx
@@ -8,11 +8,13 @@ const Error = () => {
     <div className="pt-20">
       <h1 className="text-center text-4xl my-10">
         Oooh! Snap! <br />
-        Something went wrong
+        Something broke
       </h1>
+
       <div className=" max-w-screen-md max-h-screen mx-auto">
         <Lottie animationData={animationData} loop={true} />
       </div>
+      <h3 className="text-3xl my-4 text-center">Please refresh</h3>
       <h3 className="bg-blue-500 px-8 py-2 rounded-sm shadow-md w-fit mx-auto text-blue-100 text-xl">
         Reset
       </h3>
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
 "use client";
 import LandingHero from "@/app/ui/components/LandingHero";
-import Lottie from "lottie-react";
-import animationData from "@/public/animations/wave.json";
+
 export default function Home() {
   return (
-    <section>
-      <div className="flex md:flex-row flex-col pt-20 items-center ">
-        <div className="w-full m-10 ">
-          <div className="">
-            <p className="text-5xl h-20 w-20 text-[#EB455F] font-semibold">
-              Hola!{" "}
-              <span className="">
-                <Lottie animationData={animationData} loop={true} />
-              </span>
-            </p>
-          </div>
-          <h1 className="text-2xl my-8 mx-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-            dicta!
-          </h1>
-
-          <p className="hidden md:block m-10 text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eius in
-            nesciunt quos cupiditate inventore quaerat odio dolorem similique
-            mollitia illum blanditiis voluptatibus consectetur libero vitae
-            recusandae fugit necessitatibus beatae optio, fugiat accusantium ut.
-            Tenetur praesentium eum pariatur. Vel, maiores saepe illum veritatis
-            veniam eos ea excepturi odio. Laudantium, voluptas?
-          </p>
-        </div>
-
-        <div className="">
-          <LandingHero />
-        </div>
+    <section className="container">
+      <div>
+        <LandingHero />
       </div>
     </section>
   );
