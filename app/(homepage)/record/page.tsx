490abeeb9ea020a3e8862ba9ee071b04fd0f5925
fayekelmith <faye.12222883@lpu.in>
2024-01-03 11:20:28 +0530

    to continue with achievement and admin

--- app/(homepage)/record/page.tsx
+++ app/(homepage)/record/page.tsx
@@ -1,5 +1,9 @@
 const record = () => {
-  return <div>My Achiements</div>;
+  return (
+    <div className="pt-20 mx-auto text-center">
+      <div className="text-4xl">Coming Soon</div>
+    </div>
+  );
 };
 
 export default record;
